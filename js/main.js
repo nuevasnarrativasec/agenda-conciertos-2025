589ec696cdcdf8a480909e46059680d36c9ac810
armando.scargglioni@gmail.com <armando.scargglioni@gmail.com>
2025-06-02 09:37:16 -0500

    update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        initialSlide: 4
+        initialSlide: 5
     });
 
     /* var $slider = $('.slider-fechas').slick({
