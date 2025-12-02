a91c33672746780bd10156b0e920d615db4f249c
nuevasnarrativasec <nuevasnarrativasec@gmail.com>
2025-12-02 09:54:36 -0500

    cambio mes diciembre

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         speed: 500,
         fade: true,
         cssEase: 'linear',
-        initialSlide: 7
+        initialSlide: 11
     });
 
     /* var $slider = $('.slider-fechas').slick({
